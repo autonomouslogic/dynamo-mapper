38863b1e0d80fa41c4e6f37874f3b365ce0c3494
Kenneth Jorgensen <kenneth@autonomouslogic.com>
2022-05-15 17:50:42 +0900

    Release scripts.

---
+++ publish.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -ex
+
+# Workaround for annotated tags.
+# https://github.com/semantic-release/semantic-release/pull/1871#issuecomment-980015434
+# https://github.com/autonomouslogic/semantic-release-gradle-sonatype-example/issues/5
+VERSION="$1"
+git tag -a -f -m $VERSION $VERSION
+git push --force origin $VERSION
+
+./gradlew clean
+
+./gradlew publishToSonatype closeSonatypeStagingRepository
+# Use the command below to automatically release.
+# ./gradlew publishToSonatype closeAndReleaseSonatypeStagingRepository
