600e401c80573af1eac37de4596b33a736fbded1
Kenneth Jorgensen <kenneth@autonomouslogic.com>
2022-06-11 11:51:21 +0900

    ci: defense against accidental development versions.

--- publish.sh
+++ publish.sh
@@ -5,10 +5,9 @@
 # https://github.com/autonomouslogic/semantic-release-gradle-sonatype-example/issues/5
 VERSION="$1"
 git tag -a -f -m $VERSION $VERSION
-./gradlew properties | grep version
-git tag -l -n
 if ./gradlew properties | grep version | grep -q -; then
   echo Cannot publish development version
+  ./gradlew properties | grep version
   git tag -d $VERSION
   git push origin :refs/tags/$VERSION
   exit 1
